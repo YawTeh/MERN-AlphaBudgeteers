d091de29349043583e89e0d9c5770b83545cc181
Yaw Teh <yawteh@gmail.com>
2021-08-07 02:06:55 -0400

    Created Database and Connected MongoDB

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,11 @@
 const express = require("express");
 const dotenv = require("dotenv");
 const morgan = require("morgan");
+const connectDB = require("./config/db");
 
 dotenv.config();
+
+connectDB();
 
 const transactions = require("./routes/transactionRoutes");
 
