a203b8edaeba7e033b5ecdc02f29cb1d725ceeb3
Yaw Teh <yawteh@gmail.com>
2021-08-07 16:55:35 -0400

    Prepped App for Deployment

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,4 @@
+const path = require("path");
 const express = require("express");
 const dotenv = require("dotenv");
 const morgan = require("morgan");
@@ -18,6 +19,14 @@ if (process.env.NODE_ENV === "development") {
 }
 
 app.use("/api/v1/transactions", transactions);
+
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static("frontend/build"));
+
+    app.get("*", (req, res) =>
+        res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"))
+    );
+}
 
 const PORT = process.env.PORT || 50000;
 
